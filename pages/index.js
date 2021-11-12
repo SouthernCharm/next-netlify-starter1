f6b5ffccb6368aaa1b88713b4e45e593b3192b4a
SouthernCharm <89693479+SouthernCharm@users.noreply.github.com>
2021-11-12 16:34:35 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import "./styles/reset.css";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
 
 export default function Home() {
   return (
